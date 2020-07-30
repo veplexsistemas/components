04ed67713cd8e5178eff12ac195f12c0e056e9b2
Matheus da Silva Lutz <matheus.lutz@veplex.com.br>
2020-07-30 15:16:12 -0300

    Ajustando composer - ServiceProvider

--- src/VeplexComponentsServiceProvider.php
+++ src/VeplexComponentsServiceProvider.php
@@ -6,6 +6,16 @@ use Illuminate\Support\ServiceProvider;
 
 class VeplexComponentsServiceProvider extends ServiceProvider
 {
+  /**
+    * Register services.
+    *
+    * @return void
+    */
+  public function register()
+  {
+    //
+  }
+
   public function boot()
   {
 	  $this->publishes([__DIR__.'/../views' => resource_path('views/vendor/components')]);
