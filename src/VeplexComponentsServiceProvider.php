147f30405746b3e11ba92437aa2774ce6a71629b
Matheus da Silva Lutz <matheus.lutz@veplex.com.br>
2020-07-30 16:21:00 -0300

    Teste - colocando view na vendor

--- src/VeplexComponentsServiceProvider.php
+++ src/VeplexComponentsServiceProvider.php
@@ -13,6 +13,6 @@ class VeplexComponentsServiceProvider extends ServiceProvider
 
   public function boot()
   {
-    $this->publishes([__DIR__.'/../views' => resource_path('views/components')]);
+    $this->publishes([__DIR__.'/../views' => resource_path('views/vendor/veplex')]);
   }
 }
